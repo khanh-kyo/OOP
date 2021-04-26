071b2deb894c640d0db30d3747a37b3da29bfd53
khanh-kyo <76611593+khanh-kyo@users.noreply.github.com>
2021-04-26 14:13:05 +0700

    Initial commit

---
+++ exercises_4/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OOP.exercises_4;
+import java.util.*;
+/**
+ *
+ * @author KhanhKyo
+ */
+public class Main {
+     public static void main(String[] args) {
+         
+         Scanner sc = new Scanner(System.in);
+		String[] worker = new String[10];
+                int[] salary = new int[10];
+		for (int i = 0; i < 10; i++) {
+			
+		}
+     }
+    
+}
---
+++ school/Classes.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OOP.school;
+
+/**
+ *
+ * @author KhanhKyo
+ */
+public class Classes {
+    int ID;
+    String name;
+}
